d899e3a1dd0e0f465bc952dbb8f5bb8ed2367e2d
Rustam <r.n.makulov@gmail.com>
2014-03-16 22:53:25 +0400

    small correct class name

--- runtime/src/main/java/operators/typeAttributeConvertation/NoaccessOp.java
+++ runtime/src/main/java/operators/typeAttributeConvertation/NoaccessOp.java
@@ -4,10 +4,14 @@ import psObjects.PSObject;
 import psObjects.values.simple.Operator;
 import psObjects.values.simple.PSName;
 
-/**
- * Created by Дмитрий on 16.03.14.
- */
-public class NoaccessOp extends Operator {
+public class NoAccessOp extends Operator {
+
+    public static final NoAccessOp instance = new NoAccessOp();
+
+    protected NoAccessOp() {
+        super();
+    }
+
     @Override
     public void execute() {
         PSObject o = runtime.popFromOperandStack();
